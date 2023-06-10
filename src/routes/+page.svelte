2801683243ce6d2f5bbcfaec1be1320794a19e27
Ben Cooper <cooperben83@gmail.com>
2023-06-10 15:42:22 -0400

    updated appbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,13 @@
 	import { LightSwitch, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="fixed top-0 w-full">
-	<AppBar class="shadow-lg">
-		<svelte:fragment slot="lead"
-			><h1 class="gradient-heading uppercase text-xl select-none">Blob Blocks</h1></svelte:fragment
-		>
-		<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
-	</AppBar>
-</div>
+<AppBar class="shadow-lg">
+	<svelte:fragment slot="lead"
+		><h1 class="gradient-heading uppercase text-xl select-none">Blob Blocks</h1></svelte:fragment
+	>
+	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+</AppBar>
+
 <Game />
 
 <style>
